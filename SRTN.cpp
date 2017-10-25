5c5897c11ba46e7660a47884a2a8abd5a21c8751
Rematchka <reem.abdelsalam13@gmail.com>
2017-10-25 14:26:54 +0200

    SRTN

--- SRTN.cpp
+++ SRTN.cpp
@@ -56,7 +56,7 @@ class pcb
     }
     else if(newstate==2) //stopped process
        {laststopTime=curtime;
-       remainingTime=curtime-executionStart;
+       remainingTime=runtime-(curtime-executionStart);
         }
      else
      {
@@ -125,12 +125,12 @@ public:
          if(fptr!=NULL&&process.state==3)
 
      { rewind(fptr);
-  fprintf(fptr, "At time %d process %d %s arr %d total %d remain %d wait %d TA %d WTA %.2f \n",process.executionStart,process.pid,states[process.state],process.arrivalTime,process.runtime,process.remainingTime,wait,ta,wta);
+  fprintf(fptr, "At time %d process %d %s arr %d total %d remain %d wait %d TA %d WTA %.2f \n",process.executionStart,process.id,states[process.state],process.arrivalTime,process.runtime,process.remainingTime,wait,ta,wta);
 fclose(fptr);
 }
           else if(fptr!=NULL)
           {  rewind(fptr);
- fprintf(fptr, "At time %d process %d %s arr %d total %d remain %d wait %d \n",process.executionStart,process.pid,states[process.state],process.arrivalTime,process.runtime,process.remainingTime,wait);
+ fprintf(fptr, "At time %d process %d %s arr %d total %d remain %d wait %d \n",process.executionStart,process.id,states[process.state],process.arrivalTime,process.runtime,process.remainingTime,wait);
  fclose(fptr);
 }
 
@@ -165,7 +165,7 @@ struct compare
 
  pcb runningprocess;
  
-
+  bool running=false;
  //initiate a logger class for all the running processes
 
 logger schedulerLogger("schedulerlog.txt");
@@ -197,17 +197,38 @@ void my_sigchld_handler(int sig)
         
 
           if(runningprocess.laststopTime==0) //first time to start
-          {
-            runningprocess.updatepcb(0,getClk());
+          {  
+             int newpid=fork();
+             if(newpid==-1)
+                  
+              {
+                perror("process not created\n");
+              }
+             else if(newpid==0)
+               {
+                  execl("./process.out","./process.out",(char*)NULL);
+               }
+
+             runningprocess.executionStart=getClk();
+             runningprocess.updatepcb(0,getClk());
+             runningprocess.pid=newpid;
+            running=true;
           }
           else 
           {
             runningprocess.updatepcb(1,getClk()); //resumed process
+            kill(runningprocess.pid,SIGCONT);
+             running=true;
           }
           
           schedulerLogger.logProcess(runningprocess,getClk()); //log the resuming or the start of a process
         }
+        else
+        {
+           running=false;
+         }
       }
+ 
              break; //Break after handling process termination
     }
 }
@@ -225,7 +246,7 @@ int main(int argc, char* argv[]) {
     signal(SIGCHLD,my_sigchld_handler);
 
     bool finished=false;
-
+  
     struct processData pD;
     while(!finished)
     {   int x= getClk();
@@ -234,7 +255,7 @@ int main(int argc, char* argv[]) {
     //////To receive something from the generator, for example  id 2
      int y=Recmsg(pD);
      printf("y%d\n",y); 
-    if(y==-1) printf("failure to receive data\n"); 
+    if(y==-1) printf("Nothing sent\n"); 
     else if(y==0)  //The scheduler received a new process
     {
       printf("received data\n"); 
@@ -244,13 +265,16 @@ int main(int argc, char* argv[]) {
  ///////you have to check if there is initially running processs////////////////////////////////
      //check if the received process has a shorter runtime than the currently runnig process
 
-     if(newprocess.remainingTime<runningprocess.remainingTime)
+     if(!(running)||(newprocess.remainingTime<runningprocess.remainingTime))
      {
          //STOP THE RUNNING PROCESS AND UPDATE PCB TO STATE 2=>STOPPED
+     if(running)
+      {
       kill(runningprocess.pid,SIGTSTP);
       runningprocess.updatepcb(2,getClk());
       processTable.push(runningprocess);  //Return the stopped process to the table of ready processes
       schedulerLogger.logProcess(runningprocess,getClk());  //Log the stopping of the currently running process to the file
+      }
 
         //START THE NEWLY ARRIVED PROCESS , SET ITS START OF EXECUTION AND UPDATE NEW PROCESS PCB TO STATE
         //FORK THE NEW PROCESS
@@ -263,18 +287,23 @@ int main(int argc, char* argv[]) {
          else if(newpid==0)
          {  printf("forked\n");
           stringstream s;
-          s<<runningprocess.remainingTime;
+          s<<newprocess.remainingTime;
           execl("./process.out","./process.out",s.str().c_str());
          }
-    
+      running=true;
+      newprocess.pid=newpid;
       newprocess.updatepcb(0,getClk());  //it has just started running
       runningprocess=newprocess;
       schedulerLogger.logProcess(runningprocess,getClk()); //log the start of a new process to the file
 
      }
-     else
+     else 
      {
+         if(running)
          processTable.push(newprocess);
+ 
+       
+
 
          //TODO: check if you need to log something here 
           
